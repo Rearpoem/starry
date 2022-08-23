12dcc9930e04614d025d9779e3fe60f138cce331
Rearpoem <1648723303@qq.com>
2022-08-23 09:38:09 +0800

    首页

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,58 +1,46 @@
-import React from 'react';
+import starry from "../../images/starry.jpg"
 import Typed from "typed.js";
 import bk from './bk.css';
+import { Breadcrumb, Layout, Menu } from 'antd';
+import React from 'react';
+import Typer from "../Typer";
+const { Header, Content, Footer } = Layout;
+
+
 
 const Index = (props) => {
-
-    // Create reference to store the DOM element containing the animation
-    const el = React.useRef(null);
-    // Create reference to store the Typed instance itself
-    const typed = React.useRef(null);
-
-    React.useEffect(() => {
-        const options = {
-            strings: [
-                '如果愿望可以实现，我仍想与你共度余生'
-            ],
-            typeSpeed: 100,
-            backSpeed: 50,
-        };
-
-        // elRef refers to the <span> rendered below
-        typed.current = new Typed(el.current, options);
-
-        return () => {
-            // Make sure to destroy Typed instance during cleanup
-            // to prevent memory leaks
-            typed.current.destroy();
-        }
-    }, [])
+    const starryUrl =starry;
 
     return (
-        <div>
-            <div className="wrap">
-                <div>
-                    <div  ref={el} />
-                </div>
-                {/*<button onClick={() => typed.current.toggle()}>Toggle</button>*/}
-                {/*<button onClick={() => typed.current.start()}>Start</button>*/}
-                {/*<button onClick={() => typed.current.stop()}>Stop</button>*/}
-                {/*<button onClick={() => typed.current.reset()}>Reset</button>*/}
-                {/*<button onClick={() => typed.current.destroy()}>Destroy</button>*/}
-            </div>
-        </div>
 
-    );
-    // return(
-    //     <div>
-    //     <div className={bk.bk}></div>
-    //     {/*    <span style={{background:"red",}}>1</span>*/}
-    //     </div>
-    // )
+        <Layout className="layout">
+            <Header>
+                FHun Starry
+            </Header>
+            <Content
+                style={{height:"100%"}}
+            >
+                <body
+                    background = {starryUrl}
+                    style={{padding: 0,height:1000,width:"100%",display: "flex",alignItems:"center",justifyContent:"center"}}
+                >
+                {/*<div style={{color:"white",fontSize:80,margin:"auto"}}>如果愿望可以实现，我仍想与你共度余生</div>*/}
+                <Typer/>
+                </body>
+            </Content>
+            <Footer
+                style={{
+                    textAlign: 'center',
+                }}
+            >
+                FHun Starry
+            </Footer>
+        </Layout>
 
+
+    )
 
 }
-
 
 
 export default Index;
